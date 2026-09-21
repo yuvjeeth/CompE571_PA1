31371362cfbd5368f86bad205887de3a9c49b37a
YJ <YJ@SDSU>
2026-09-21 14:39:13 -0700

    We're instructed to use the sum function only (thanks Abhi for pointing this out)

--- main.c
+++ main.c
@@ -2,21 +2,17 @@
 
 int main(void)
 {
-	long n = 10000000000, i = 0, j = 0, numPrime = 0;
-    printf("Elon Musk ki jai\n");
-    while(i <= n)
+	unsigned long n = 1000000, i = 0, sum = 0;
+    printf("Launch sequence initiated!\n");
+    while(i < n)
     {
-        for(j = 2; j < i; j++)
-        {
-            if(j % i == 0){
-                continue;
-                printf("%ld is not a prime number.\n", i);
-            }
-            else{
-                numPrime++;
-                printf("------%ld is a prime number------\n", i);
-            }
-        }
+        sum += i;
+        i++;
+        //Uncomment this while debugging
+        // if(i % 1000 == 0){
+        //     printf("We're at %lu right now...\n",i);
+        // }
     }
+    printf("It's the finish line! The sum is %lu\n", sum);
 	return 0;
 }
