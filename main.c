db9066d9a06942cab051955eb8803fd60adf806a
YJ <YJ@SDSU>
2026-09-21 11:46:27 -0700

    Prime numbers as baseline problem

--- main.c
+++ main.c
@@ -2,6 +2,21 @@
 
 int main(void)
 {
-	printf("Hello, World!\n");
+	long n = 10000000000, i = 0, j = 0, numPrime = 0;
+    printf("Elon Musk ki jai\n");
+    while(i <= n)
+    {
+        for(j = 2; j < i; j++)
+        {
+            if(j % i == 0){
+                continue;
+                printf("%ld is not a prime number.\n", i);
+            }
+            else{
+                numPrime++;
+                printf("------%ld is a prime number------\n", i);
+            }
+        }
+    }
 	return 0;
 }
