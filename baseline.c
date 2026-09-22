da97d92b4c9070a9479c3646aab8b3fde1241433
Abhishek Raj Mishra <83797556+Abhishek2078@users.noreply.github.com>
2026-09-21 17:32:40 -0700

    Refactor baseline.c for prime number logic
    Refactor main function to include prime number checking and remove unused code.

--- baseline.c
+++ baseline.c
@@ -1,40 +1,22 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-void print_int128(__int128_t number)//holding larger integer of sum in different function
+#include <stdio.h>
+
+int main(void)
 {
-    if(number>=10)
+	long n = 10000000000, i = 0, j = 0, numPrime = 0;
+    printf("Elon Musk ki jai\n");
+    while(i <= n)
     {
-        print_int128(number/10);
+        for(j = 2; j < i; j++)
+        {
+            if(j % i == 0){
+                continue;
+                printf("%ld is not a prime number.\n", i);
+            }
+            else{
+                numPrime++;
+                printf("------%ld is a prime number------\n", i);
+            }
+        }
     }
-    printf("%d", (int)(number%10));
-}
-int main(int argc,char *argv[])// argc take two argument
-{// argv take character one base line one input
-    if(argc !=2)// it will check the condition if its correct it will proceed further
-    {
-        printf("Please provideN\n");
-        return 1;
-
-    }
-    __int128_t sum=0;// assing sum =0;
-    struct  timespec start;
-    struct timespec end;
-    clock_gettime(CLOCK_MONOTONIC,&start);
-    double elapsed;
-    long long N=atoll(argv[1]);// atoll will change character to numbers 
-     for(long long i=0;i<N;i++)
-     {
-        sum +=i;
-     }
-      clock_gettime(CLOCK_MONOTONIC,&end);
-    // printf("N =%lld\n", N);
-     //printf("sum=%lld\n", sum);// it will print sum
-     printf("Expected+=");
-     print_int128(sum);
-     printf("\n");
-
-     elapsed=(end.tv_sec-start.tv_sec)+(end.tv_nsec-start.tv_nsec)/1000000000.0;
-     printf("Time=%.6f seconds\n", elapsed);
-    return 0;
+	return 0;
 }
