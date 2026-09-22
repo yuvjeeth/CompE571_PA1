28dbadad959aba588cead13fe11c4864a7343c74
Abhishek Raj Mishra <83797556+Abhishek2078@users.noreply.github.com>
2026-09-21 17:04:26 -0700

    Improve handling of large integers and timing
    Refactor main function and add print_int128 for large sums.

--- baseline.c
+++ baseline.c
@@ -1,18 +1,40 @@
-#include <stdio.h>
-
-int main(void)
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void print_int128(__int128_t number)//holding larger integer of sum in different function
 {
-	unsigned long n = 1000000, i = 0, sum = 0;
-    printf("Launch sequence initiated!\n");
-    while(i < n)
+    if(number>=10)
     {
-        sum += i;
-        i++;
-        //Uncomment this while debugging
-        // if(i % 1000 == 0){
-        //     printf("We're at %lu right now...\n",i);
-        // }
+        print_int128(number/10);
     }
-    printf("It's the finish line! The sum is %lu\n", sum);
-	return 0;
+    printf("%d", (int)(number%10));
+}
+int main(int argc,char *argv[])// argc take two argument
+{// argv take character one base line one input
+    if(argc !=2)// it will check the condition if its correct it will proceed further
+    {
+        printf("Please provideN\n");
+        return 1;
+
+    }
+    __int128_t sum=0;// assing sum =0;
+    struct  timespec start;
+    struct timespec end;
+    clock_gettime(CLOCK_MONOTONIC,&start);
+    double elapsed;
+    long long N=atoll(argv[1]);// atoll will change character to numbers 
+     for(long long i=0;i<N;i++)
+     {
+        sum +=i;
+     }
+      clock_gettime(CLOCK_MONOTONIC,&end);
+    // printf("N =%lld\n", N);
+     //printf("sum=%lld\n", sum);// it will print sum
+     printf("Expected+=");
+     print_int128(sum);
+     printf("\n");
+
+     elapsed=(end.tv_sec-start.tv_sec)+(end.tv_nsec-start.tv_nsec)/1000000000.0;
+     printf("Time=%.6f seconds\n", elapsed);
+    return 0;
 }
